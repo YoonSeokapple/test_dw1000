2cf9b0033da08d70562077cf0d734b2d05141c97
YoonSeokapple <158424095+YoonSeokapple@users.noreply.github.com>
2024-02-05 11:00:35 +0900

    Arduino + DW1000

---
+++ Arduino + DW1000/examples/5.StandardRTLSTag_TWR/5.StandardRTLSTag_TWR.ino
@@ -0,0 +1,159 @@
+/*
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+
+/* 
+ * StandardRTLSTag_TWR.ino
+ * 
+ * This is an example tag in a RTLS using two way ranging ISO/IEC 24730-62_2013 messages
+ */
+
+#include <DW1000Ng.hpp>
+#include <DW1000NgUtils.hpp>
+#include <DW1000NgTime.hpp>
+#include <DW1000NgConstants.hpp>
+#include <DW1000NgRanging.hpp>
+#include <DW1000NgRTLS.hpp>
+
+// connection pins
+#if defined(ESP8266)
+const uint8_t PIN_SS = 15;
+#else
+const uint8_t PIN_SS = 10; // spi select pin
+const uint8_t PIN_RST = 7;
+#endif
+
+// Extended Unique Identifier register. 64-bit device identifier. Register file: 0x01
+const char EUI[] = "AA:BB:CC:DD:EE:FF:00:00";
+
+volatile uint32_t blink_rate = 200;
+
+double range_A;
+double range_B;
+double range_C;
+
+typedef struct Position {
+    double x;
+    double y;
+} Position;
+
+Position position_A = {0,0};
+Position position_B = {1.2,0};
+Position position_C = {1.2,1.2};
+
+
+device_configuration_t DEFAULT_CONFIG = {
+    false,
+    true,
+    true,
+    true,
+    false,
+    SFDMode::STANDARD_SFD,
+    Channel::CHANNEL_5,
+    DataRate::RATE_850KBPS,
+    PulseFrequency::FREQ_16MHZ,
+    PreambleLength::LEN_256,
+    PreambleCode::CODE_3
+};
+
+frame_filtering_configuration_t TAG_FRAME_FILTER_CONFIG = {
+    false,
+    false,
+    true,
+    false,
+    false,
+    false,
+    false,
+    false
+};
+
+sleep_configuration_t SLEEP_CONFIG = {
+    false,  // onWakeUpRunADC   reg 0x2C:00
+    false,  // onWakeUpReceive
+    false,  // onWakeUpLoadEUI
+    true,   // onWakeUpLoadL64Param
+    true,   // preserveSleep
+    true,   // enableSLP    reg 0x2C:06
+    false,  // enableWakePIN
+    true    // enableWakeSPI
+};
+
+void setup() {
+    // DEBUG monitoring
+    Serial.begin(115200);
+    Serial.println(F("### DW1000Ng-arduino-ranging-tag ###"));
+    // initialize the driver
+    #if defined(ESP8266)
+    DW1000Ng::initializeNoInterrupt(PIN_SS);
+    #else
+    DW1000Ng::initializeNoInterrupt(PIN_SS, PIN_RST);
+    #endif
+    Serial.println("DW1000Ng initialized ...");
+    // general configuration
+    DW1000Ng::applyConfiguration(DEFAULT_CONFIG);
+    DW1000Ng::enableFrameFiltering(TAG_FRAME_FILTER_CONFIG);
+    
+    DW1000Ng::setEUI(EUI);
+
+    DW1000Ng::setNetworkId(RTLS_APP_ID);
+
+    DW1000Ng::setAntennaDelay(16436);
+
+    DW1000Ng::applySleepConfiguration(SLEEP_CONFIG);
+
+    DW1000Ng::setPreambleDetectionTimeout(64);
+    DW1000Ng::setSfdDetectionTimeout(273);
+    DW1000Ng::setReceiveFrameWaitTimeoutPeriod(2960);
+    
+    Serial.println(F("Committed configuration ..."));
+    // DEBUG chip info and registers pretty printed
+    char msg[128];
+    DW1000Ng::getPrintableDeviceIdentifier(msg);
+    Serial.print("Device ID: "); Serial.println(msg);
+    DW1000Ng::getPrintableExtendedUniqueIdentifier(msg);
+    Serial.print("Unique ID: "); Serial.println(msg);
+    DW1000Ng::getPrintableNetworkIdAndShortAddress(msg);
+    Serial.print("Network ID & Device Address: "); Serial.println(msg);
+    DW1000Ng::getPrintableDeviceMode(msg);
+    Serial.print("Device mode: "); Serial.println(msg);    
+
+}
+
+void calculatePosition(double &x, double &y) {
+
+    /* This gives for granted that the z plane is the same for anchor and tags */
+    double A = ( (-2*position_A.x) + (2*position_B.x) );
+    double B = ( (-2*position_A.y) + (2*position_B.y) );
+    double C = (range_A*range_A) - (range_B*range_B) - (position_A.x*position_A.x) + (position_B.x*position_B.x) - (position_A.y*position_A.y) + (position_B.y*position_B.y);
+    double D = ( (-2*position_B.x) + (2*position_C.x) );
+    double E = ( (-2*position_B.y) + (2*position_C.y) );
+    double F = (range_B*range_B) - (range_C*range_C) - (position_B.x*position_B.x) + (position_C.x*position_C.x) - (position_B.y*position_B.y) + (position_C.y*position_C.y);
+
+    x = (C*E-F*B) / (E*A-B*D);
+    y = (C*D-A*F) / (B*D-A*E);
+}
+
+void loop() {
+
+    double x,y;
+    DW1000Ng::setEUI(EUI);
+    RangeInfrastructureResult_v2 res = DW1000NgRTLS::tagTwrLocalize_v2(1500);
+
+    if(res.main_dist && res.b_dist && res.c_dist)
+    {
+      range_A = res.main_dist;
+      range_B = res.b_dist;
+      range_C = res.c_dist;
+      
+      calculatePosition(x,y);
+      Serial.print("X : ");
+      Serial.println(x);
+      Serial.print("Y : ");
+      Serial.println(y);
+      Serial.println("\n");
+    }
+}
